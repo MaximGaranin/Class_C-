6bb637efccca617c7bac6b0de66a9b00eff24ced
MaximGaranin <lmibuh3@yandex.ru>
2026-05-06 22:08:21 +0400

    Fix

--- obj/Debug/net8.0/Class.AssemblyInfo.cs
+++ obj/Debug/net8.0/Class.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Class")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12ecd694fae2afcf6943fbd012c4417e65a56b07")]
 [assembly: System.Reflection.AssemblyProductAttribute("Class")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Class")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
